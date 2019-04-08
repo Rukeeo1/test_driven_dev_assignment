d52f3b7d4f23776b4b1b01f738b78d69d465c867
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-04-08 21:54:27 +0100

    function now adds evevery element in the arguments array

--- indefinite_num_sum/indefinite_sum.js
+++ indefinite_num_sum/indefinite_sum.js
@@ -1,3 +1,12 @@
 const indefiniteObj = {
-    
+    sumEveryThing: function(){
+        var s = 0;
+        for (var i=0; i < arguments.length; i++) {
+            s += arguments[i];
+        }
+        return s;
+        }
 }
+
+console.log(indefiniteObj.sumEveryThing(1,2,3,4));
+module.exports = indefiniteObj;
---
+++ indefinite_num_sum/indefinite_sum.test.js
@@ -0,0 +1,5 @@
+const sumAll = require('./indefinite_sum');
+
+describe('sum all Numbers', function(){
+    // it('argume')
+});
