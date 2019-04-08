6aae1972925c14c4c9ad4db05bfc1cb9afedd5ad
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-04-08 23:55:29 +0100

    all parameters must be numbers diff

--- find_diff_twonums/diff.js
+++ find_diff_twonums/diff.js
@@ -1,7 +1,12 @@
 //Takes two numbers as argument and return the difference between the numbers
 const diffObj = {
     diff: function(a,b){
-        return a - b;
+        if(typeof a !== 'number' && typeof b !== 'number'){
+            return 'all parameter\'s must be numbers'
+        }
+        let x = a - b;
+        console.log(x)
+        return x;
     }
 }
 
--- find_diff_twonums/diff.test.js
+++ find_diff_twonums/diff.test.js
@@ -4,5 +4,11 @@ describe('returns the difference between two numbers', function(){
   it('should return the difference between two numbers', function(){
     expect(subTwo.diff(2,2)).toBe(0);
   });  
+
+  it('all paramaters passed in must be numbers',function(){
+    expect(subTwo.diff('2')).toBe('you must pass in two parameters');
+  });
 });
+
+//string like numbers can be converted to numbers...
 
