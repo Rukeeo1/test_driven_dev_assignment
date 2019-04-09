d6e77a2c4b1d15403adca8047878f0582abbd0d5
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-04-09 14:10:52 +0100

    concantenate_two_strings: peforms basic concat and does not allow non string

--- concantenate_two_strings/concatenate.js
+++ concantenate_two_strings/concatenate.js
@@ -1,5 +1,8 @@
 const addTwoStrings = {
     concat: function(a,b){
+        if(typeof a !== 'string' || typeof b !== 'string'){
+            return 'all parameters must be string';
+        }
         console.log(a+b);
         return a + b;
     }
--- concantenate_two_strings/concatenate.test.js
+++ concantenate_two_strings/concatenate.test.js
@@ -4,4 +4,10 @@ describe('should concatenate two strings', function(){
     it('should add two strings', function(){
         expect(strConcat.concat('ruk','ee')).toBe('rukee');
     });
+
+    it('should ignore non strings', function(){
+        expect(strConcat.concat('rukee', {})).toBe('all parameters must be string');
+        expect(strConcat.concat('rukee', [])).toBe('all parameters must be string');
+        expect(strConcat.concat(2,'hello')).toBe('all parameters must be string');
+    });
 });
